40ad89fa1b3133969a812c532a2692006da87554
Phat <phatdroid94@gmail.com>
2023-02-07 20:13:24 +0700

    var dynamic

--- bin/dart_fundamental02022023.dart
+++ bin/dart_fundamental02022023.dart
@@ -1,19 +1,24 @@
 void main() {
   // 1: Kiểu dữ liệu
   // String kiểu chuỗi
-  String name = "Pham Tấn Phát";
+  // String name = "Pham Tấn Phát";
   // int: Số nguyên, double: Số thực
-  int age = 29;
-  double weight = 74.2;
-  num undefinedNumber = 10;
+  // int age = 29;
+  // double weight = 74.2;
+  // num undefinedNumber = 10;
   // bool: kiểu chỉ 2 miền giá trị true hoặc false
-  bool isMale = true;
+  // bool isMale = true;
   // runes: kiểu Unicode character
   // String text1 = '\u00AE';
   // ctrl + p: xem tham số truyền vào
-  Runes runesTemplate = Runes(
-      "\u{2651} \u{2652} \u{2653} \u{2654}"
-  );
+  // Runes runesTemplate = Runes(
+  //     "\u{2651} \u{2652} \u{2653} \u{2654}"
+  // );
+  //
+  // print(runesTemplate.string);
 
-  print(runesTemplate.string);
+  // 2: Khai báo biến
+  // var value = 10;
+  // dynamic value2 = "abc";
+  // value2 = Runes("a");
 }
