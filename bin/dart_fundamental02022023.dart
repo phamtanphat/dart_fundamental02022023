1a2033649a3062edc2541abe0c5099d5dfd972ac
Phat <phatdroid94@gmail.com>
2023-02-07 20:00:54 +0700

    Initial commit

---
+++ bin/dart_fundamental02022023.dart
@@ -0,0 +1,13 @@
+void main() {
+  // 1: Kiểu dữ liệu
+  // String kiểu chuỗi
+  String name = "Pham Tấn Phát";
+  // int: Số nguyên, double: Số thực
+  int age = 29;
+  double weight = 74.2;
+  num undefinedNumber = 10;
+  // bool: kiểu chỉ 2 miền giá trị true hoặc false
+  bool isMale = true;
+  // runes: kiểu Unicode character
+  String text1 = '\u00AE';
+}
