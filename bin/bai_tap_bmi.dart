0a8fb281f4711f1c4a70d7b2d48b046f41eb154f
Phat <phatdroid94@gmail.com>
2023-02-07 21:16:14 +0700

    bai tap ve nha

---
+++ bin/bai_tap_bmi.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  calculateBMI(1.75, 75);
+}
+
+// function
+void calculateBMI(double height, double weight) {
+  // Công thức: weight / height * height
+  // Yêu cầu:
+  //      Chỉ số cơ thể = ?
+  //      Tình trạng sức khỏe = ?
+}
