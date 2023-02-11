13dce6bedcaf4a9de8fe6f45f56649955d5285a5
Phat <phatdroid94@gmail.com>
2023-02-11 19:56:03 +0700

    property vs constructor

--- bin/animal.dart
+++ bin/animal.dart
@@ -1,4 +1,12 @@
 class Animal {
   // 1: Thuoc tinh (property)
+  late String name;
+  late double weight;
   // 2: Phuong thuc (function)
+
+  // constructor (Phuong thuc khoi tao)
+  Animal(String name, double weight) {
+    this.name = name;
+    this.weight = weight;
+  }
 }
